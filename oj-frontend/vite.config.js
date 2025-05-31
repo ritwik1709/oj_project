27669be25f158706561574aa1c66e1a99a9612a5
Ritwik Sudhakar Tat <ritwikt17@gmail.com>
2025-06-01 03:47:29 +0530

    Update vite.config.js

--- oj-frontend/vite.config.js
+++ oj-frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 5173,
+    strictPort: true,
+    allowedHosts: [
+      'localhost',
+      'online-judge-frontend-1jhg.onrender.com',
+      'online-judge-frontend.onrender.com'
+    ]
+  },
+  server: {
+    host: true,
+    port: process.env.PORT || 5173,
+    strictPort: true
+  }
 })
